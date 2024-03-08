c093a4b9c8d8403f54ac9745b3074f4882e5d0d8
Sandeep Joshi <joshisandip111@gmail.com>
2024-03-08 15:31:03 +0545

    added new resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect } from "react";
-import CV from "../../assest/Sandeep  CV.pdf";
+import CV from "../../assest/Sandeep CV.pdf";
 import AOS from "aos";
 import "aos/dist/aos.css";
 const CTA = () => {
   useEffect(() => {
     AOS.init({ duration: 2000 });
+    
   }, []);
+
+  
   return (
     <div className="cta" data-aos="fade-up" data-aos-duration="2000">
-      <a href={CV} download className="btn">
+      <a
+        href={CV}
+        target="_blank"
+        className="btn"
+        title="Sandeep CV.pdf"
+        rel="noreferrer"
+      >
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
